4a653c338e4f58fd9dfbf4aeec38faa736287307
cwade <cwade@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-07-07 05:17:01 +0000

    fixes/updates
    git-svn-id: http://svn.peeklinux.com/trunk@398 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

--- peek-build/src/emobiix/Exeapi.c
+++ peek-build/src/emobiix/Exeapi.c
@@ -8,6 +8,11 @@
 extern int TCD_Interrupt_Level;
 ExeTaskCbT *ExeTaskCb[EXE_NUM_TASKS];
 
+
+typedef struct {
+        uint32 masks[EXE_NUM_MAILBOX];
+}sMailQueueSig;
+
 //NU_MEMORY_POOL *ExeSystemMemory;
 //uint32		*ExeIntStackP; // this is some structure we need to work out
 
@@ -87,11 +92,17 @@ void CallExeFault(void) {
 	MonFault(MON_EXE_FAULT_UNIT, 3, 0, MON_HALT);
 }
 
+// needs work
 ExeEventWaitT ExeEventRetireve( ExeTaskIdT TaskId, uint32 RequestEvent, uint32 Timeout )
 {
-	uint32 *ret_flags = 0;
-	// Need to get group_ptr from ExeTaskCb
+	int ret;
+	ExeTaskCbT *task;
 	//EVCE_Retrieve_Events(,,1/* operation */, RequestEvent);
+
+        task = ExeTaskCb[TaskId];
+
+	ret_flags = EVCE_Retrieve_Events(&task->EventGroupCb, signal_mask, NU_OR_CONSUME, &signal_mask, suspend);
+
 
 }
 	
@@ -111,6 +122,8 @@ void ExeSignalSet(ExeTaskIdT TaskId, ExeSignalT SignalFlg)
 {
         NU_HISR  *cHISR = TCC_Current_HISR_Pointer();
         NU_TASK  *cTCTP;
+	ExeTaskCbT *task;
+
 
         if(!cHISR) {
                 if(!(cTCTP = TCC_Current_Task_Pointer())) {
@@ -118,9 +131,10 @@ void ExeSignalSet(ExeTaskIdT TaskId, ExeSignalT SignalFlg)
                 }
         }
 
-	//if(EVCE_Set_Events(XXX, SignalFlg, 0)) { // Needs to lookup group_ptr in ExeTaskCb
-	//	CallExeFault();
-	//}
+	task = ExeTaskCb[TaskId]; 
+	
+        if (EVCE_Set_Events(&task->EventGroupCb, EXE_SIGNAL_TYPE | SignalFlg, 0)) 
+		MonFault(MON_EXE_FAULT_UNIT, 3, 0, MON_HALT);
 
 	return;
 }
@@ -387,153 +401,54 @@ void ExeMemoryPoolDelete(NU_MEMORY_POOL *pool) {
 	DMCE_Delete_Memory_Pool(pool);
 }
 
-typedef struct {
-	uint32 masks[EXE_NUM_MAILBOX];
-}sMailQueueSig;
-
-
 ExeEventWaitT ExeEventWait(ExeTaskIdT TaskId, bool Signal, ExeMessageT Message, uint32 Timeout)
 {
-uint32 suspend;
-uint32 signal_mask;
-uint32 timeout;
-int ret;
-ExeTaskCbT *task;
-int i;
-sMailQueueSig MailQueueSig = { EXE_MAILBOX_1, EXE_MAILBOX_2, EXE_MAILBOX_3, EXE_MAILBOX_4, EXE_MAILBOX_5 };
+	uint32 suspend;
+	uint32 signal_mask;
+	uint32 timeout;
+	int ret;
+	ExeTaskCbT *task;
+	int i;
+	sMailQueueSig MailQueueSig = { EXE_MAILBOX_1, EXE_MAILBOX_2, EXE_MAILBOX_3, EXE_MAILBOX_4, EXE_MAILBOX_5 };
+	timeout = Timeout;
+	signal_mask = 0;
 
-timeout = Timeout;
-//                PUSH    {R0-R7,LR}
-//                STR     R3, [SP,#0x24+timeout]
 
-signal_mask = 0;
-//                MOVS    R7, #0
-//                STR     R7, [SP,#0x24+signal_mask]
 
-if (Signal == EXE_SIGNAL_TRUE)
-	signal_mask = ~0xFD; // EXE_SIGNAL_1 ... EXE_SIGNAL_23 | EXE_SIGNAL_TYPE
+	if (Signal == EXE_SIGNAL_TRUE)
+		signal_mask = ~0xFD; // EXE_SIGNAL_1 ... EXE_SIGNAL_23 | EXE_SIGNAL_TYPE
 
-//                CMP     R1, #1
-//                BNE     L1
-//                MOVS    R1, #0xFD ; '²'
-//                MVNS    R1, R1
-//                STR     R1, [SP,#0x24+signal_mask]
+	if (Message == EXE_MESSAGE_TRUE)
+		signal_mask |= (EXE_MESSAGE_TYPE | EXE_MAILBOX_1 | EXE_MAILBOX_2 | EXE_MAILBOX_3 | EXE_MAILBOX_4 | EXE_MAILBOX_5);
+	else
+		signal_mask |= Message;
 
-//L1:                                      ; CODE XREF: $ExeEventWait+Aj
-if (Message == EXE_MESSAGE_TRUE)
-{
-	signal_mask |= (EXE_MESSAGE_TYPE | EXE_MAILBOX_1 | EXE_MAILBOX_2 | EXE_MAILBOX_3 | EXE_MAILBOX_4 | EXE_MAILBOX_5);
-}
-//                CMP     R2, #1
-//                BEQ     L2
-//                LDR     R1, [SP,#0x24+signal_mask]
-//                ORRS    R1, R2
-//                STR     R1, [SP,#0x24+signal_mask]
-//               B       L3
-//; ---------------------------------------------------------------------------
-else 
-{
-	signal_mask |= Message;
-}
-//L2:                                      ; CODE XREF: $ExeEventWait+14j
-//                MOV     R1, SP
-//                LDRB    R3, [R1,#0x24+signal_mask]
-//                MOVS    R2, #0xF9 ; '·'
-//                ORRS    R2, R3
-//                STRB    R2, [R1,#0x24+signal_mask]
-
-//L3:                                      ; CODE XREF: $ExeEventWait+1Cj
-task = ExeTaskCb[TaskId];  // extern /*const*/ ExeTaskCbT     *ExeTaskCb[];
-//                LSLS    R0, R0, #2
-//                LDR     R1, =_ExeTaskCb
-//                ADDS    R6, R1, R0
-//                LDR     R1, [R6]
-//                LDR     R0, [R1]
-if (task->NumMsgs)
-{
-//                CMP     R0, #0
-//                BLE     L6
-//                MOVS    R5, #5 
-//                ADDS    R4, R7, #0 
-	for (i = 0; i < EXE_NUM_MAILBOX; i++) 
-	{ 
-//L4:                                      ; CODE XREF: $ExeEventWait+64j
-//                ADDS    R0, R4, R1
-//                LDR     R0, [R0,#4]
-		if (!task->NumMsgsInQueue[i])
-			continue;
-//                CMP     R0, #0
-//                BLE     L5
-//                MOVS    R0, 0x228  
-//                ADDS    R0, R0, R1
-//                LDR     R1, =_MailQueueSig 
-//                LDR     R1, [R1,R4]
-//                MOVS    R2, #1 
-//                ORRS    R1, R2  
-//                MOVS    R2, R7
-
-//STATUS          EVCE_Set_Events(NU_EVENT_GROUP *group_ptr, UNSIGNED events, OPTION operation); 
-		if (EVCE_Set_Events(&task->EventGroupCb, MailQueueSig.masks[i] | EXE_MESSAGE_TYPE, 0))
-			CallExeFault();
-
-//                BL      $EVCE_Set_Events
-//                CMP     R0, #0
-//                BEQ     L5
-//                BL      $CallExeFault
-
-//L5:                                      ; CODE XREF: $ExeEventWait+40j
-//                                        ; $ExeEventWait+58j
-//                ADDS    R4, #4 
-//                LDR     R1, [R6]
-//                SUBS    R5, #1
-//                BNE     L4
+	task = ExeTaskCb[TaskId];
+	if (task->NumMsgs)
+	{
+		for (i = 0; i < EXE_NUM_MAILBOX; i++) 
+		{ 
+			if (!task->NumMsgsInQueue[i])
+				continue;
+			if (EVCE_Set_Events(&task->EventGroupCb, MailQueueSig.masks[i] | EXE_MESSAGE_TYPE, 0))
+				CallExeFault();
+		}
 	}
-}
 
-//L6:                                      ; CODE XREF: $ExeEventWait+34j
-suspend = timeout;
-//                LDR     R0, [SP,#0x24+timeout]
-//                STR     R0, [SP,#0x24+suspend]
-//                MOVS    R0, 0x228
-//               ADDS    R0, R0, R1
-//               LDR     R1, [SP,#0x24+signal_mask]
-//                MOVS    R2, #1
-//                ADD     R3, SP, #0x24+signal_mask
+	suspend = timeout;
+	ret = EVCE_Retrieve_Events(&task->EventGroupCb, signal_mask, NU_OR_CONSUME, &signal_mask, suspend);
 
-// im not seeing it...
-//STATUS EVCE_Retrieve_Events(NU_EVENT_GROUP *group_ptr, UNSIGNED requested_flags, OPTION operation, UNSIGNED *retrieved_flags, UNSIGNED suspend);
-ret = EVCE_Retrieve_Events(&task->EventGroupCb, signal_mask, NU_OR_CONSUME, &signal_mask, suspend);
+	switch (ret)
+	{
+		case NU_TIMEOUT:
+			return EXE_TIMEOUT_TYPE;
 
-switch (ret)
-{
-	case NU_TIMEOUT:
-		return EXE_TIMEOUT_TYPE;
+		case NU_SUCCESS:
+			return signal_mask;
 
-	case NU_SUCCESS:
-		return signal_mask;
-
-	default:
+	    default:
 		CallExeFault();
-}
+	}
 
-return suspend;
-//                BL      $EVCE_Retrieve_Events
-//                MOVS    R4, R0 // ret == 0
-//                BEQ     L8   // success
-//                MOVS    R1, R0  // ret != 0
-//                ADDS    R1, #0x32 ; '2'  // ret + 32 == 0? 
-//                BEQ     L7  // timeout
-//                BL      $CallExeFault
-
-//L7:                                      ; CODE XREF: $ExeEventWait+82j
-//                ADDS    R4, #0x32 ; '2' 
-//                BNE     L8              
-//                MOVS    R0, #4
-//                STR     R0, [SP,#0x24+signal_mask]
-
-//L8:                                      ; CODE XREF: $ExeEventWait+7Cj
-//                                        ; $ExeEventWait+8Aj
-//                LDR     R0, [SP,#0x24+signal_mask]
-//                STR     R0, [SP,#0x24+suspend]
-//                POP     {R0-R7,PC}
+	return suspend;
 }
