cae485a18ee59df2d7ad81b39287ef0e45b6c42f
ryan <ryan@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-07-13 14:58:12 +0000

    FFS implementation of emo File api
    git-svn-id: http://svn.peeklinux.com/trunk@435 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

---
+++ peek-build/src/emobiix/FFSFile.c
@@ -0,0 +1,137 @@
+#include "File.h"
+
+#include "ffsapi.h"
+
+struct File_t
+{
+    FsiHandleT handle;
+    char *filename;
+};
+
+/*typedef enum {FP_START, FP_END, FP_CURRENT} FilePosition;*/
+
+File *file_openRead(const char *filename)
+{
+    File *output;
+    FsiResultT result;
+
+    output = (File *)p_malloc(sizeof(File));
+    if (output == NULL)
+        return NULL;
+    result = FsiFileOpen(&output->handle, filename, FSI_FILE_OPEN_READ_EXIST);
+    if (result != FSI_SUCCESS) {
+        emo_printf("Failed to open %s reason %d\n", filename, result);
+        p_free(output);
+    }
+    output->filename = p_strdup(filename);
+    return output;
+}
+
+File *file_openWrite(const char *filename)
+{
+    File *output;
+    FsiResultT result;
+
+    output = (File *)p_malloc(sizeof(File));
+    if (output == NULL)
+        return NULL;
+    result = FsiFileOpen(&output->handle, filename, FSI_FILE_OPEN_WRITE_ALWAYS);
+    if (result != FSI_SUCCESS) {
+        emo_printf("Failed to open %s reason %d\n", filename, result);
+        p_free(output);
+    }
+    return output;
+}
+
+File *file_openAppend(const char *filename)
+{
+    File *output;
+    FsiResultT result;
+
+    output = (File *)p_malloc(sizeof(File));
+    if (output == NULL)
+        return NULL;
+    result = FsiFileOpen(&output->handle, filename, FSI_FILE_OPEN_WRITE_EXIST);
+    if (result != FSI_SUCCESS) {
+        emo_printf("Failed to open %s reason %d\n", filename, result);
+        p_free(output);
+    }
+    file_seek(output, 0, FP_END);
+    return output;
+}
+
+int file_read(File *f, int bytes, void *buffer)
+{
+    uint32 readbytes;
+    FsiResultT result;
+
+    readbytes = bytes;
+    result = FsiFileRead(buffer, 1, &readbytes, &f->handle);
+    if (result == 0)
+        return (int)readbytes;
+    return -1;
+}
+
+int file_write(File *f, int bytes, void *buffer)
+{
+    uint32 writebytes;
+    FsiResultT result;
+
+    writebytes = bytes;
+    result = FsiFileRead(buffer, 1, &writebytes, &f->handle);
+    if (result == 0)
+        return (int)writebytes;
+    return -1;
+}
+
+int file_seek(File *f, int offset, FilePosition pos)
+{
+    FsiFileSeekTypeT from;
+    switch (pos) {
+        case FP_START: from = FSI_FILE_SEEK_START; break;
+        case FP_END: from = FSI_FILE_SEEK_END; break;
+        case FP_CURRENT:
+        default: from = FSI_FILE_SEEK_CURRENT; break;
+    }
+    return FsiSeek(&f->handle, , offset);
+}
+
+int file_pos(File *f)
+{
+    uint32 pos;
+    FsiTesultT result;
+    
+    result = FsiTell(&f->handle, &pos);
+    if (result == 0)
+        return (int)pos;
+    return -1;
+}
+
+int file_eof(File *f)
+{
+
+}
+
+int file_size(File *f)
+{
+    FsiResult result;
+    uint32 length;
+
+    result = FsiGetFileLength(f->filename, &length);
+    if (result == 0)
+        return (int)length;
+    return -1;
+}
+
+int file_close(File *f)
+{
+    FsiResult result;
+
+    result = FsiFileClose(&f->handle)
+    p_free(f->filename);
+    p_free(f);
+
+    return result;
+}
+
+#endif /* _FILE_H_ */
