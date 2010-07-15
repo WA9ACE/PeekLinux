de6fb1802f9a0486a3fddcc87ad1d0d1bea02d21
ryan <ryan@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-07-15 01:22:22 +0000

    Added static applications
    git-svn-id: http://svn.peeklinux.com/trunk@461 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

---
+++ peek-build/src/emobiix/StaticApplications.c
@@ -0,0 +1,53 @@
+#include "BootApplication.h"
+
+#include "DataObject.h"
+#include "Widget.h"
+#include "URL.h"
+
+#include <stdio.h>
+
+#include "p_malloc.h"
+
+#include "Mime.h"
+
+/* in BootApplication.c */
+extern DataObject *dobjFromFile(const char *filename, DataObject *root);
+
+DataObject *LockApplication(void)
+{
+	static DataObject *output = NULL;
+	DataObject *dobj1, *root, *setw;
+
+	if (output != NULL)
+        return output;
+
+    output = dataobject_new();
+    dataobject_setValue(output, "type", dataobjectfield_string("application"));
+    dataobject_setValue(output, "name", dataobjectfield_string("Lock Screen"));
+    dataobject_setValue(output, "description",
+            dataobjectfield_string("Lock screen for mobile"));
+    dataobject_setValue(output, "startupview",
+            dataobjectfield_string("mainview"));
+	dataobject_setValue(output, "fullscreen",
+            dataobjectfield_string("true"));
+ 
+	root = widget_newTypeIdName("view", NULL, "mainview", output);
+	widget_setPacking(root, WP_VERTICAL);
+
+	dobj1 = dobjFromFile("street64.png", root);
+	widget_setPacking(dobj1, WP_VERTICAL);
+
+	setw = widget_newTypeIdName("label", "biglabelw", NULL, dobj1);
+	dataobject_setValue(setw, "data", dataobjectfield_string("1:15p"));
+	dataobject_setValue(setw, "weight", dataobjectfield_string("bold"));
+	dataobject_setValue(setw, "marginleft", dataobjectfield_string("20"));
+	dataobject_setValue(setw, "margintop", dataobjectfield_string("15"));
+
+	setw = widget_newTypeIdName("label", "labelw", NULL, dobj1);
+	dataobject_setValue(setw, "data", dataobjectfield_string("Friday, Dec 17 2009"));
+	dataobject_setValue(setw, "weight", dataobjectfield_string("bold"));
+	dataobject_setValue(setw, "marginleft", dataobjectfield_string("20"));
+	dataobject_setValue(setw, "margintop", dataobjectfield_string("4"));
+
+	return output;
+}
---
+++ peek-build/src/emobiix/StaticApplications.h
@@ -0,0 +1,16 @@
+#ifndef _STATICAPPLICATIONS_H_
+#define _STATICAPPLICATIONS_H_
+
+#include "DataObject_internal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DataObject *LockApplication(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
