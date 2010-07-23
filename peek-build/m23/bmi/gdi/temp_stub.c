11a8d338c4be93deae907933b6c7383e5a32da98
cwade <cwade@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-07-23 03:46:59 +0000

    updated to include task in scroll and increased buffer
    git-svn-id: http://svn.peeklinux.com/trunk@487 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

--- peek-build/m23/bmi/gdi/temp_stub.c
+++ peek-build/m23/bmi/gdi/temp_stub.c
@@ -2,6 +2,7 @@
 #include "rvf_api.h"
 #include "rv_trace.h"
 #include "rvm_use_id_list.h"
+#include "nucleus.h"
 
 void BalKeypadGetNotify(void)
 {
@@ -9,34 +10,61 @@ void BalKeypadGetNotify(void)
 }
 
 void bal_trace(const char* fmt, ...) {
-        char buf[1024];
+        char buf[10000];
         va_list ap;
-        va_start(ap, fmt);
-        vsprintf(buf, fmt, ap);
-        rvf_send_trace (buf,strlen(buf),NULL_PARAM,RV_TRACE_LEVEL_DEBUG_HIGH,RVM_USE_ID )
+        NU_TASK *tp;
 
+        va_start(ap, fmt);
+        tp =  TCC_Current_Task_Pointer();
+
+        if(!tp) {
+                vsprintf(buf, fmt, ap);
+        } else {
+                sprintf(buf, "Task [%s] ", tp->tc_name);
+                vsprintf(buf+strlen(buf), fmt, ap);
+        }
+        rvf_send_trace (buf,strlen(buf)+1,NULL_PARAM,RV_TRACE_LEVEL_DEBUG_HIGH,RVM_USE_ID )
         va_end(ap);
+        TCCE_Task_Sleep(2);
 }
 
 void bal_printf( const char* fmt, ...) {
-        char buf[1024];
+        char buf[10000];
         va_list ap;
-        va_start(ap, fmt);
-        vsprintf(buf, fmt, ap);
-        rvf_send_trace (buf,strlen(buf),NULL_PARAM,RV_TRACE_LEVEL_DEBUG_HIGH,RVM_USE_ID )
+        NU_TASK *tp;
 
+        va_start(ap, fmt);
+        tp =  TCC_Current_Task_Pointer();
+
+        if(!tp) {
+                vsprintf(buf, fmt, ap);
+        } else {
+                sprintf(buf, "Task [%s] ", tp->tc_name);
+                vsprintf(buf+strlen(buf), fmt, ap);
+        }
+        rvf_send_trace (buf,strlen(buf)+1,NULL_PARAM,RV_TRACE_LEVEL_DEBUG_HIGH,RVM_USE_ID )
         va_end(ap);
+        TCCE_Task_Sleep(2);
 }
 
 //unsigned int mfwMmeDrvFlag;
 
 
 void emo_printf( const char* fmt, ...) {
-	char buf[1024];
+	char buf[10000];
         va_list ap;
+	NU_TASK *tp;
+
         va_start(ap, fmt);
-        vsprintf(buf, fmt, ap);
-  	rvf_send_trace (buf,strlen(buf),NULL_PARAM,RV_TRACE_LEVEL_DEBUG_HIGH,RVM_USE_ID )
+	tp =  TCC_Current_Task_Pointer();
+
+	if(!tp) {
+		vsprintf(buf, fmt, ap);
+	} else {
+		sprintf(buf, "Task [%s] ", tp->tc_name);
+		vsprintf(buf+strlen(buf), fmt, ap);
+	}
+  	rvf_send_trace (buf,strlen(buf)+1,NULL_PARAM,RV_TRACE_LEVEL_DEBUG_HIGH,RVM_USE_ID )
         va_end(ap);
-	TCCE_Task_Sleep(1);
+	TCCE_Task_Sleep(2);
 }
