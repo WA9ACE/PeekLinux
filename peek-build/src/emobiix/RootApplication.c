2a4b0f8ce7a592c58187beaec0fa4c0060a19688
cwade <cwade@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-08-01 19:51:32 +0000

    change top bar color
    git-svn-id: http://svn.peeklinux.com/trunk@518 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

--- peek-build/src/emobiix/RootApplication.c
+++ peek-build/src/emobiix/RootApplication.c
@@ -56,7 +56,9 @@ DataObject *RootApplication(void)
 	root = widget_newTypeIdName("view", NULL, "rootview", output);
 	widget_setPacking(root, WP_VERTICAL);
 
-	dobj1 = widget_newTypeIdName("box", "gradbox", NULL, root);
+	dobj1 = widget_newTypeIdName("box", "solid", NULL, root);
+	dataobject_setValue(dobj1, "color", dataobjectfield_string("66666600"));
+
 	dataobject_setValue(dobj1, "width", dataobjectfield_string("320"));
 	dataobject_setValue(dobj1, "height", dataobjectfield_string("25"));
 	widget_setPacking(dobj1, WP_HORIZONTAL);
