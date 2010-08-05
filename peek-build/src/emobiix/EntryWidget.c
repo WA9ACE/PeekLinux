0fae10bab100cfbfdf8ed22e6470c14106e44c39
ryan <ryan@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-08-05 15:44:39 +0000

    use styles for properties on entry. devolve font down to single in anticipation of new font manager Misc fixes for more robust handling of text
    git-svn-id: http://svn.peeklinux.com/trunk@575 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

--- peek-build/src/emobiix/EntryWidget.c
+++ peek-build/src/emobiix/EntryWidget.c
@@ -56,28 +56,45 @@ int entryWidget_handleKey(Widget *w, int key, Style *s)
 	return 1;
 }
 
+extern Font *defaultFont;
+
 /* entry renderer */
 static void entry_renderer(WidgetRenderer *wr, Style *s, Widget *w,
 		DataObject *dobj) {
 	Rectangle *box, *margin;
 	DataObject *text;
 	DataObjectField *field, *cursor, *startindex, *multiline;
+	/*Style *style;*/
 	Font *f;
-	Color c, background, line;
-	const char *dtype;
-	const char *ltype;
+	Color c;/*, background, line;*/
+	/*const char *dtype;
+	const char *ltype;*/
 	const char *str;
-	int percent, offset;
+	int percent, offset;/*, hasFocus;*/
+	WidgetRenderer *iwr;
 
 	box = widget_getBox(w);
 	margin = widget_getMargin(w);
-	dtype = (const char *)dataobject_getValue(dobj, "type")->field.string;
-	ltype = widget_getID(w);
-	f = (Font *)style_getProperty(s, NULL, ltype, dtype, "font");
-	c.value = (unsigned int)style_getProperty(s, NULL, ltype, dtype, "color");
-	background.value = (unsigned int)style_getProperty(s, NULL, ltype, dtype, "background");
-	if (widget_hasFocus(w))
-		line.value = (unsigned int)style_getProperty(s, NULL, ltype, dtype, "focusline");
+	/*dtype = (const char *)dataobject_getValue(dobj, "type")->field.string;
+	ltype = widget_getID(w);*/
+
+	iwr = NULL;
+	style_getRenderer(s, w, "box-renderer", &iwr);
+	if (iwr != NULL)
+		iwr->render(wr, s, w, dobj);
+#if 0
+	hasFocus = widget_hasFocus(w);
+	style = style_getID(s, ltype, hasFocus);
+	f = (Font *)defaultFont;/*style_getProperty(s, NULL, ltype, dtype, "font");*/
+	/*c.value = (unsigned int)style_getProperty(s, NULL, ltype, dtype, "color");*/
+	c.value = 0;
+	style_getColor(s, w, "color", &c.value);
+	/*background.value = (unsigned int)style_getProperty(s, NULL, ltype, dtype, "background");*/
+	background.value = 0;
+	style_getColor(s, w, "background", &background.value);
+	/*line.value = (unsigned int)style_getProperty(s, NULL, ltype, dtype, "focusline");*/
+	if (hsaFocus)
+		style_getColor(s, w, "focusline", &line.value);
 	else
 		line.value = (unsigned int)style_getProperty(s, NULL, ltype, dtype, "line");
 	text = widget_getDataObject(w);
@@ -87,6 +104,19 @@ static void entry_renderer(WidgetRenderer *wr, Style *s, Widget *w,
 	} else {
 		str = "";
 	}
+#endif
+
+	text = widget_getDataObject(w);
+	field = dataobject_getValue(text, "data");
+	if (field != NULL) {
+		str = field->field.string;
+	} else {
+		str = "";
+	}
+
+	f = (Font *)defaultFont;
+	c.value = 0;
+	style_getColor(s, w, "font-color", &c.value);
 
 	cursor = dataobject_getValueAsInt(w, "cursor");
 	if (cursor == NULL) {
@@ -106,15 +136,15 @@ static void entry_renderer(WidgetRenderer *wr, Style *s, Widget *w,
 		dataobject_setValue(w, "multiline", multiline);
 	}
 
-	lgui_roundedbox_fill(box->x+margin->x, box->y+margin->y, box->width-1, box->height-1, 7,
+	/*lgui_roundedbox_fill(box->x+margin->x, box->y+margin->y, box->width-1, box->height-1, 7,
 			background.rgba.red, background.rgba.green, background.rgba.blue);
 	lgui_roundedbox_line(box->x+margin->x, box->y+margin->y, box->width-1, box->height-1, 7,
-			line.rgba.red, line.rgba.green, line.rgba.blue);
+			line.rgba.red, line.rgba.green, line.rgba.blue);*/
 
 	lgui_complex_draw_font(box->x+4+margin->x, box->y+margin->y, box->width-9, box->height, str,
 			f, c, cursor->field.integer, startindex->field.integer, &percent, &offset, 0);
 
-	if (multiline->type == DOF_STRING && strcmp(multiline->field.string, "true") == 0) {
+	if (dataobjectfield_isTrue(multiline)) {
 		lgui_vline(box->x+margin->x+box->width-8, box->y+margin->y+1, box->height-1, 4, 0x44, 0x44, 0xFF);
 		lgui_vline(box->x+margin->x+box->width-8, box->y+margin->y+1+offset*(box->height-1)/100,
 				percent*(box->height-1)/100, 4, 0xCC, 0xCC, 0xFF);
@@ -128,11 +158,17 @@ static void entry_measure(WidgetRenderer *wr, Style *s, Widget *w,
 	const char *dtype;
 	const char *ltype;
 	const char *str;
+	DataObjectField *field;
 
 	dtype = (const char *)dataobject_getValue(dobj, "type")->field.string;
 	ltype = widget_getID(w);
-	f = (Font *)style_getProperty(s, NULL, ltype, dtype, "font");
-	str = (const char *)dataobject_getValue(dobj, "data")->field.data.bytes;
+	f = (Font *)defaultFont;
+	field = dataobject_getValue(dobj, "data");
+	if (field == NULL) {
+		field = dataobjectfield_string("");	
+		dataobject_setValue(dobj, "data", field);
+	}
+	str = (const char *)field->field.data.bytes;
 
 	lgui_measure_font(str, f, 0, p);
 	p->y += 4;
