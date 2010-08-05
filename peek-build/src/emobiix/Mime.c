17b9187c953298f6976a604c3d945f56a6553b27
ryan <ryan@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-08-05 14:29:09 +0000

    set data pointer to NULL after we free it to prevent accidental misuse
    git-svn-id: http://svn.peeklinux.com/trunk@553 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

--- peek-build/src/emobiix/Mime.c
+++ peek-build/src/emobiix/Mime.c
@@ -231,6 +231,7 @@ static int load_png(DataObject *dobj)
 	dataobject_setValue(dobj, "data", dataobjectfield_data(convertedOutput, outSize));
 
 	p_free(data->field.data.bytes);
+	data->field.data.bytes = NULL;
 	data->field.data.size = 0;
 
 	return 1;
