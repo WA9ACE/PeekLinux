1c736e1bf92bfb1451d81da68b9fd7b23322855d
ryan <ryan@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-08-05 14:28:13 +0000

    wrap memory handlers for tracking allocations in simulation
    git-svn-id: http://svn.peeklinux.com/trunk@552 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

--- peek-build/src/emobiix/p_malloc.h
+++ peek-build/src/emobiix/p_malloc.h
@@ -5,12 +5,30 @@
 extern "C" {
 #endif
 
+#ifdef SIMULATOR
+
+void *p_malloc_i(int s, const char *filename, int line,
+        const char *function);
+void p_free_i(void *, const char *filename, int line,
+        const char *function);
+void *p_realloc_i(void *, int newsize, const char *filename, int line,
+        const char *function);
+void *p_calloc_i(int elem, int size, const char *filename, int line,
+        const char *function);
+char *p_strdup_i(const char *s, const char *filename, int line,
+        const char *function);
+#define p_malloc(s) p_malloc_i(s, __FILE__, __LINE__, __FUNCTION__)
+#define p_free(s) p_free_i(s, __FILE__, __LINE__, __FUNCTION__)
+#define p_realloc(s, n) p_realloc_i(s, n, __FILE__, __LINE__, __FUNCTION__)
+#define p_calloc(s, n) p_calloc_i(s, n, __FILE__, __LINE__, __FUNCTION__)
+#define p_strdup(s) p_strdup_i(s, __FILE__, __LINE__, __FUNCTION__)
+#else
 void *p_malloc(int);
-void *p_calloc(int elems, int size);
 void p_free(void *);
 void *p_realloc(void *, int newsize);
-
+void *p_calloc(int elems, int size);
 char *p_strdup(const char *s);
+#endif
 
 #ifdef __cplusplus
 }
