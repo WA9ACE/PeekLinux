4d492462e8504a93c8277833b57e505f409b461a
ryan <ryan@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-08-05 16:45:11 +0000

    moved system specific key mapping out of application manager
    git-svn-id: http://svn.peeklinux.com/trunk@583 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

--- peek-build/src/emobiix/ApplicationManager.c
+++ peek-build/src/emobiix/ApplicationManager.c
@@ -22,11 +22,15 @@
 
 #include "p_malloc.h"
 
+#include "KeyMappings.h"
+
+#if 0
 #ifdef SIMULATOR
 #include "sim-keymap.h"
 #else
 #include "mfw_kbd.h"
 //#include "KeyMap.h"
+#endif
 #endif
 
 #include <string.h>
@@ -118,13 +122,17 @@ void manager_drawScreenPartial(void)
 	DataObject *view;
 	Style *style;
 
-        emo_printf("manager_drawScreenPartial()0");
+#ifndef SIMULATOR
+    emo_printf("manager_drawScreenPartial()0");
+#endif
 
 	if (appManager->rootApplication == NULL)
 		return;
 
 	view = application_getCurrentScreen(appManager->rootApplication);
+#ifndef SIMULATOR
 	emo_printf("manager_drawScreenPartial()1");
+#endif
 	if (view != NULL) {
 		style = application_getCurrentStyle(appManager->focus);
 		if (style == NULL)
@@ -156,78 +164,7 @@ void manager_handleKey(int key)
 
 	emo_printf("Got Key[%d]\n", key);
 
-#ifndef SIMULATOR
-	switch (key) {
-		case KCD_0: mappedKey = '0'; break;
-		case KCD_1: mappedKey = '1'; break;
-		case KCD_2: mappedKey = '2'; break;
-		case KCD_3: mappedKey = '3'; break;
-		case KCD_4: mappedKey = '4'; break;
-		case KCD_5: mappedKey = '5'; break;
-		case KCD_6: mappedKey = '6'; break;
-		case KCD_7: mappedKey = '7'; break;
-		case KCD_8: mappedKey = '8'; break;
-		case KCD_9: mappedKey = '9'; break;
-	/*
-		 case KCD_a: mappedKey = 'a'; break;
-		 case KCD_b: mappedKey = 'b'; break;
-		 case KCD_c: mappedKey = 'c'; break;
-		 case KCD_d: mappedKey = 'd'; break;
-		 case KCD_e: mappedKey = 'e'; break;
-		 case KCD_f: mappedKey = 'f'; break;
-		 case KCD_g: mappedKey = 'g'; break;
-		 case KCD_h: mappedKey = 'h'; break;
-		 case KCD_i: mappedKey = 'i'; break;
-		 case KCD_j: mappedKey = 'j'; break;
-		 case KCD_k: mappedKey = 'k'; break;
-		 case KCD_l: mappedKey = 'l'; break;
-		 case KCD_m: mappedKey = 'm'; break;
-		 case KCD_n: mappedKey = 'n'; break;
-		 case KCD_o: mappedKey = 'o'; break;
-		 case KCD_p: mappedKey = 'p'; break;
-		 case KCD_q: mappedKey = 'q'; break;
-		 case KCD_r: mappedKey = 'r'; break;
-		 case KCD_s: mappedKey = 's'; break;
-		 case KCD_t: mappedKey = 't'; break;
-		 case KCD_u: mappedKey = 'u'; break;
-		 case KCD_v: mappedKey = 'v'; break;
-		 case KCD_w: mappedKey = 'w'; break;
-		 case KCD_x: mappedKey = 'x'; break;
-		 case KCD_y: mappedKey = 'y'; break;
-		 case KCD_z: mappedKey = 'z'; break;
-	 */
-		case KCD_A: mappedKey = 'A'; break;
-		case KCD_B: mappedKey = 'B'; break;
-		case KCD_C: mappedKey = 'C'; break;
-		case KCD_D: mappedKey = 'D'; break;
-		case KCD_E: mappedKey = 'E'; break;
-		case KCD_F: mappedKey = 'F'; break;
-		case KCD_G: mappedKey = 'G'; break;
-		case KCD_H: mappedKey = 'H'; break;
-		case KCD_I: mappedKey = 'I'; break;
-		case KCD_J: mappedKey = 'J'; break;
-		case KCD_K: mappedKey = 'K'; break;
-		case KCD_L: mappedKey = 'L'; break;
-		case KCD_M: mappedKey = 'M'; break;
-		case KCD_N: mappedKey = 'N'; break;
-		case KCD_O: mappedKey = 'O'; break;
-		case KCD_P: mappedKey = 'P'; break;
-		case KCD_Q: mappedKey = 'Q'; break;
-		case KCD_R: mappedKey = 'R'; break;
-		case KCD_S: mappedKey = 'S'; break;
-		case KCD_T: mappedKey = 'T'; break;
-		case KCD_U: mappedKey = 'U'; break;
-		case KCD_V: mappedKey = 'V'; break;
-		case KCD_W: mappedKey = 'W'; break;
-		case KCD_X: mappedKey = 'X'; break;
-		case KCD_Y: mappedKey = 'Y'; break;
-		case KCD_Z: mappedKey = 'Z'; break;
-		case KCD_SPACE: mappedKey = ' '; break;
-		case KCD_BACKSPACE: mappedKey = '\b'; break;
-	}
-#endif
-
-	if (key == KCD_LOCK) { /* lock symbol */
+	if (key == EKEY_ALTTAB) { /* lock symbol */
 		manager_focusNextApplication();
 		return;
 	}
@@ -260,15 +197,15 @@ void manager_handleKey(int key)
 	}
 
 	switch (key) {
-		case KCD_NAV_CENTER:
+		case EKEY_ACTIVATE:
 			script_event(focus != NULL ? focus : appManager->rootApplicationWindow, "onreturn");
 			break;
-		case KCD_UP: // up on scroll
+		case EKEY_FOCUSPREV: // up on scroll
 			/*case 103:*/ /* GLUT a */
 			widget_focusPrev(appManager->rootApplicationWindow,
 					style);
 			break;
-		case KCD_DOWN: // down on scroll
+		case EKEY_FOCUSNEXT: // down on scroll
 			/*case 104:*/ /* GLUT z */
 			widget_focusNext(appManager->rootApplicationWindow,
 					style);
