b8629dee3f062016f7e104245ee1d049422310f2
andrey <andrey@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-08-21 23:05:33 +0000

    added file wrapper for base lib functions
    git-svn-id: http://svn.peeklinux.com/trunk@793 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

---
+++ peek-build/src/netsurf/emoport/peek_file.c
@@ -0,0 +1,59 @@
+#include <file.h>
+#include "ffs.h"
+
+static int peek_open(const char *path, unsigned flags, int llv_fd)
+{
+	int openflag = 0;
+	
+	if (flags & O_RDONLY == O_RDONLY) openflag |= FFS_O_RDONLY;
+	if (flags & O_WRONLY == O_WRONLY) openflag |= FFS_O_WRONLY;
+	if (flags & O_RDWR == O_RDWR) openflag |= FFS_O_RDWR;
+	if (flags & O_APPEND == O_APPEND) openflag |= FFS_O_APPEND;
+	if (flags & O_CREAT == O_CREAT) openflag |= O_CREAT;
+	if (flags & O_TRUNC == O_TRUNC) openflag |= FFS_O_TRUNC;
+//	if (flags & O_BINARY == O_BINARY) openflag |= FFS_O_BINARY;
+
+	return ffs_open(path, flags);
+}
+
+static int peek_close(int dev_fd)
+{
+	return ffs_close(dev_fd);
+}
+
+static int peek_read(int dev_fd, char *buf, unsigned count)
+{
+	return ffs_read(dev_fd, (void *)buf, count);
+}
+
+static int peek_write(int dev_fd, const char *buf, unsigned count)
+{
+	return ffs_write(dev_fd, (void *)buf, count);
+}
+
+static off_t peek_seek(int dev_fd, off_t offset, int origin)
+{
+	int whence = 0;
+	
+	if (origin == SEEK_SET) whence = FFS_SEEK_SET;
+	else if (origin == SEEK_CUR) whence = FFS_SEEK_CUR;
+	else if (origin == SEEK_END) whence = FFS_SEEK_END;
+
+	return ffs_seek(dev_fd, offset, whence);
+}
+
+static int peek_unlink(const char *path)
+{
+	return ffs_remove(path);
+}
+
+static int peek_rename(const char *old_name, const char *new_name)
+{
+	return ffs_rename(old_name, new_name);
+}
+
+void peek_file_init(void)
+{
+	add_device("peekffs", _MSA, peek_open, peek_close, peek_read, peek_write, peek_seek, peek_unlink, peek_rename);
+}
+
