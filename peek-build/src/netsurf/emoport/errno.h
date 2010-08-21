23fcb0a6e77ad58274f8d9f38b21758183c2c799
andrey <andrey@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-08-21 06:31:26 +0000

    proper naming of system header
    git-svn-id: http://svn.peeklinux.com/trunk@775 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

---
+++ peek-build/src/netsurf/emoport/errno.h
@@ -0,0 +1,12 @@
+#ifndef __ERRNO_H__
+#define __ERRNO_H__
+
+extern int errno;
+
+#define E2BIG		7			/* Argument list too long */
+#define	ENOMEM	12		/* Out of memory */
+#define EINVAL	22		/* Invalid argument */
+#define EILSEQ	84		/* Illegal byte sequence */
+
+#endif //__ERRNO_H__
+
--- peek-build/src/netsurf/emoport/errorno.h
+++
@@ -1,2 +0,0 @@
-#define E2BIG            7      /* Argument list too long */
-#define EINVAL		 22	/* Invalid argument */
