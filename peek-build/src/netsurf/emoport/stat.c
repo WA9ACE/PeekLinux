b28fe1b6c5223051630e0d29391403ce71fbc97e
andrey <andrey@e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1>
2010-08-21 20:43:12 +0000

    adding stat
    git-svn-id: http://svn.peeklinux.com/trunk@789 e6eaa9d7-a8e6-4b40-915f-875ea51fe5d1

---
+++ peek-build/src/netsurf/emoport/stat.c
@@ -0,0 +1,33 @@
+#include <sys/stat.h>
+
+#include "relfs.h"
+#include "reliance_rtos.h"
+
+int stat(const char *path, struct stat *buf)
+{
+	int fd;
+	int ret;
+	REL_STAT st;
+
+	fd = relFs_Open(path, REL_O_RDONLY, 0x00);
+	if (fd < 0)
+		return -1;
+
+	ret = relFs_Stat(fd, &st);
+	if (ret < 0)
+		return -1;
+
+	buf->st_mode = 0;
+	if (st.uAttributes & REL_ATTR_DIRECTORY)
+		buf->st_mode = S_IFDIR;
+
+	buf->st_size = st.ulSize;
+	buf->st_atime = st.ulAccess;
+	buf->st_mtime = st.ulModify;
+	buf->st_ctime = st.ulCreation;
+	
+	relFs_Close(fd);
+
+	return 0;
+}
+
